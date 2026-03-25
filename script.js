56c154aea592c5ceebd3f3e5078ebbc1e04d40ea
mariaziui <azigamarita@gmail.com>
2026-03-25 19:16:13 -0400

    Fixed issues with hover effect

--- script.js
+++ script.js
@@ -1,30 +1,34 @@
-const leftButton = document.getElementById("left");
-const rightButton = document.getElementById("right");
+const changeSize = document.querySelector('button');
+const container = document.querySelector(".container");
 
 function createGrid(gridSize) {
-    let container = document.querySelector(".container");
     container.innerHTML = '';
     container.style.setProperty("--grid-size", gridSize);
 
-    let square;
-
     for (let i = 0; i < (gridSize*gridSize); i++) {
-        square = document.createElement('div');
+        let square = document.createElement('div');
         square.className = "square";
+
+        square.addEventListener("mouseover", () => {
+            square.style.backgroundColor = 'black';
+        });
+
         container.appendChild(square);
-    
+
     }
 }
 
 createGrid(16);
 
-const squares = document.querySelectorAll('.square');
+changeSize.addEventListener("click", () => {
+    let newSize = parseInt(prompt("Enter a number to change the size of the grid.", 16));
 
-squares.forEach(square => {
-    square.addEventListener('mouseover', () => {
-        square.style.backgroundColor = 'black';
-    });
-})
-
-
+    if (!Number.isInteger(newSize) || newSize < 2) {
+        alert("The input has to be a number between 2 and 100, please try again.");
+    } else if (newSize >= 100) {
+        alert("Grid size is too high! Enter a number between 2 and 100.");
+    } else {
+        createGrid(newSize);
+    }
+});
 
