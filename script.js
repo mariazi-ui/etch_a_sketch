017287af6b710e6eee3897cd4eda2b709af9bf51
mariaziui <azigamarita@gmail.com>
2026-03-26 13:04:00 -0400

    Added a color changing function and changed header fonts

--- script.js
+++ script.js
@@ -1,5 +1,14 @@
-const changeSize = document.querySelector('button');
+const changeSize = document.querySelector(".changeSize");
 const container = document.querySelector(".container");
+const reset = document.querySelector(".resetGrid");
+
+function getRandomRBG() {
+    let r = Math.floor(Math.random()*256);
+    let g = Math.floor(Math.random()*256);
+    let b = Math.floor(Math.random()*256);
+
+    return `rgb(${r},${g},${b})`;
+}
 
 function createGrid(gridSize) {
     container.innerHTML = '';
@@ -10,25 +19,27 @@ function createGrid(gridSize) {
         square.className = "square";
 
         square.addEventListener("mouseover", () => {
-            square.style.backgroundColor = 'black';
+            square.style.backgroundColor = getRandomRBG();
         });
 
         container.appendChild(square);
-
     }
 }
 
-createGrid(16);
+window.addEventListener("load", () => {
+    createGrid(16);
 
-changeSize.addEventListener("click", () => {
-    let newSize = parseInt(prompt("Enter a number to change the size of the grid.", 16));
+    changeSize.addEventListener("click", () => {
+        let newSize = parseInt(prompt("Enter a number to change the size of the grid.", "Number has to be between 2 - 100."));
 
-    if (!Number.isInteger(newSize) || newSize < 2) {
-        alert("The input has to be a number between 2 and 100, please try again.");
-    } else if (newSize >= 100) {
-        alert("Grid size is too high! Enter a number between 2 and 100.");
-    } else {
-        createGrid(newSize);
-    }
+        if (!Number.isInteger(newSize) || (newSize < 2 || newSize > 100)) {
+            alert("ERROR! The input has to be a number between 2 and 100, please try again.");
+        } else {
+            createGrid(newSize);
+        }
+    });
+
+    reset.addEventListener("click", () => {
+        createGrid(16);
+    })
 });
-
